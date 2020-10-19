ad1d61f677aeb0da28f101261384898e5fabb654
Ryan <Ryan@Ryans-MacBook-Pro.local>
2020-10-18 19:45:05 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/steubsengineering/cfapp/precache-manifest.606c4c65f5cfe1a466f9c852c965f089.js"
+  "/github.com/steubsengineering/cfapp/precache-manifest.1fbe25fb9f91f636356da36ebb72e75d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/steubsengineering/cfapp/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/github.com/steubsengineering/cfapp/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
